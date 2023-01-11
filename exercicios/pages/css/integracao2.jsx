c06eae9a50e9115003a038e1285c390282b54303
neanderdev <neander.faria@hotmail.com>
2023-01-11 15:54:30 -0300

    Integração com CSS #02

---
+++ exercicios/pages/css/integracao2.jsx
@@ -0,0 +1,19 @@
+import styles from './integracao2.module.css';
+
+export default function Integracao2() {
+    return (
+        <div id={styles.integracao2}>
+            <div className={styles.vermelha}>
+                Texto #01
+            </div>
+
+            <div className={styles.azul}>
+                Texto #02
+            </div>
+
+            <div className={styles.branca}>
+                Texto #03
+            </div>
+        </div>
+    );
+}
