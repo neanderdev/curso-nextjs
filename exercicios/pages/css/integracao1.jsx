eb61a74a39ed8471fad929c535e84808df8ad49b
neanderdev <neander.faria@hotmail.com>
2023-01-11 15:38:26 -0300

    Integração com CSS #01

---
+++ exercicios/pages/css/integracao1.jsx
@@ -0,0 +1,17 @@
+export default function Integracao1() {
+    return (
+        <div className="integracao1">
+            <div className="vermelha">
+                Texto #01
+            </div>
+
+            <div className="azul">
+                Texto #02
+            </div>
+
+            <div className="branca">
+                Texto #03
+            </div>
+        </div>
+    );
+}
