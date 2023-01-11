bd6892c5470f1f888a326aae0ca2d6241e6af960
neanderdev <neander.faria@hotmail.com>
2023-01-11 16:53:58 -0300

    Projeto Tabuleiro

---
+++ tabuleiro/components/Subdivisao.jsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Subdivisao.module.css';
+
+export default function Subdivisao(props) {
+    return (
+        <div 
+            className={styles.subdivisao}
+            style={{
+                backgroundColor: props.preta ? '#000' : '#FFF'
+            }}
+        ></div>
+    );
+}
