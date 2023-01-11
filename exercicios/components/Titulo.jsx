9279046a9ff3edc8b6b0b9ff93e6e460d3e47f17
neanderdev <neander.faria@hotmail.com>
2023-01-11 15:20:03 -0300

    Propriedade Componente

--- exercicios/components/Titulo.jsx
+++ exercicios/components/Titulo.jsx
@@ -1,9 +1,15 @@
-export default function Titulo() {
-    return (
+export default function Titulo(props) {    
+    return props.pequeno ? (
         <>
-            <h1>Título</h1>
+            <p>{props.principal}</p>
 
-            <h2>Subtítulo</h2>
+            <p>{props.secundario}</p>
+        </>
+    ) : (
+        <>
+            <h1>{props.principal}</h1>
+
+            <h2>{props.secundario}</h2>
         </>
     )
 }
--- exercicios/pages/basicos/usando-titulo.jsx
+++ exercicios/pages/basicos/usando-titulo.jsx
@@ -3,7 +3,22 @@ import Titulo from '../../components/Titulo';
 export default function UsandoTitulo() {
     return (
         <div>
-            <Titulo />
+            <Titulo 
+                principal="Página de Cadastro"
+                secundario="Incluir, alterar e excluir coisas!"                
+            />
+
+            <Titulo 
+                principal="Página de Login"
+                secundario="Informe o seu email e senha"
+                pequeno={true}
+            />
+
+            <Titulo
+                principal="Página de Login"
+                secundario="Informe o seu email e senha"
+                pequeno
+            />
         </div>
     );
 }
