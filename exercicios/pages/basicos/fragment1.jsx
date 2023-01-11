c2cb2bc17118be315f6f6378f953df8c16931616
neanderdev <neander.faria@hotmail.com>
2023-01-11 15:00:13 -0300

    Fragment #01

---
+++ exercicios/pages/basicos/fragment1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Fragment1() {
+    return (
+        <React.Fragment>
+            <h1>Título</h1>
+        
+            <h2>Subtítulo</h2>
+        </React.Fragment>
+    );
+}
