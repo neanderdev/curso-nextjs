d08273662365d4531572a9442c093fabf5288987
neanderdev <neander.faria@hotmail.com>
2023-01-12 15:01:32 -0300

    Comunicação indireta #02

--- exercicios/pages/estado/mouse.jsx
+++ exercicios/pages/estado/mouse.jsx
@@ -2,7 +2,15 @@ import { useState } from 'react';
 
 export default function Mouse() {
     const [x, setX] = useState(0);
-    const [y, setY] = useState(0);    
+    const [y, setY] = useState(0);
+
+    // const arrayX = useState(0);
+    // let x = arrayX[0];
+    // const alterarX = arrayX[1];
+
+    // const arrayY = useState(0);
+    // let y = arrayY[0];
+    // const alterarY = arrayY[1];
 
     const estilo = {
         display: 'flex',
